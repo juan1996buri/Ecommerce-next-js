cf904034bf93930ca66d872261dae7777b4fdd6c
juan1996buri <juan98buri@gmail.com>
2022-04-19 10:50:26 -0500

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,47 +1,91 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 import NexLink from 'next/link';
 import {
   AppBar,
   Container,
+  createMuiTheme,
+  CssBaseline,
   Link,
+  Switch,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from '@material-ui/core';
 import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createMuiTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem,',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem,',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
   return (
     <div>
       <Head>
         <title>{title ? `${title} -  Next Amazon ` : ` Next Amazon`}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NexLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>amazona</Typography>
-            </Link>
-          </NexLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NexLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NexLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>amazona</Typography>
+              </Link>
             </NexLink>
-            <NexLink href="/cart" passHref>
-              <Link>Login</Link>
-            </NexLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer>
-        <Typography className={classes.footer}>
-          All right reserved. Next
-        </Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NexLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NexLink>
+              <NexLink href="/login" passHref>
+                <Link>Login</Link>
+              </NexLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer>
+          <Typography className={classes.footer}>
+            All right reserved. Next
+          </Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
